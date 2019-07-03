6563a185314320d19ed194887b0a8abaa48b69c9
Junghyun Colin Kim <Colin.kim@groundx.xyz>
2019-07-03 14:20:48 +0900

    Delete change_ground_x.sh (#4)

--- change_ground_x.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# On macOS
-files=$(git grep -l "ground-x") && echo $files | xargs -t sed -i '' 's/ground-x/klaytn/g'
-
-# On Linux
-#files=$(git grep -l "ground-x") && echo $files | xargs -t sed -i 's/ground-x/klaytn/g'
